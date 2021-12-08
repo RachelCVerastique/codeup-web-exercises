c75ebfc48d33b29b1fb9733b70b14cabc534fb84
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2021-12-08 12:00:53 -0600

    adds js project

---
+++ js/Hello-asteroid.js
@@ -0,0 +1,7 @@
+var favoriteFood = "a chicken salad sandwich from Mcalisters";
+
+console.log("I love and cannot live without " + favoriteFood + "!!!!");
+
+var holdUpWaitAMinute = "Please HOLD THE ONIONS";
+
+console.log("I love and cannot live without " + favoriteFood + "!!!! " + holdUpWaitAMinute + "!!!");
