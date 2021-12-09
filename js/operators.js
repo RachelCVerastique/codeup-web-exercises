fdc5062cf2387982f6befcbd928361341663c762
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2021-12-08 23:18:50 -0600

    adds js project

---
+++ js/operators.js
@@ -0,0 +1,53 @@
+console.log(typeof 42);
+console.log(typeof '42');
+
+console.log(typeof true);
+console.log(1+1, 3/2, 6-3, 5*5);
+
+console.log(5%2);
+
+console.log(5%3);
+
+console.log(2+3*4);
+
+console.log((2+3) *4);
+
+console.log("Hello, " + "World!");
+
+console.log(false && false || true);
+
+console.log(false && (false || true));
+
+console.log('1' + 2);
+
+console.log(typeof '1' + 2);
+
+console.log(6 % 4);
+
+console.log('3 + 4 is ' + 3 + 4);
+
+console.log('3 + 4 is ' + (3 + 4));
+
+console.log(0 < 0);
+
+console.log('false' == false);
+
+console.log(true == 'true')
+
+console.log(5 >= -5);
+
+console.log(!false || false);
+
+console.log(true || "42");
+
+console.log(!true && !false);
+
+console.log(6 % 5);
+
+console.log('codeup' === 'codeup' && 'codeup' === 'Codeup');
+
+console.log(4 >= 0 && 1 !== '1');
+
+console.log(6 % 3 === 0);
+
+console.log(5 % 2 !== 0);
