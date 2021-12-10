72e2b7e4050d724578b0192cfb3103eb79068bfc
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2021-12-10 16:37:52 -0600

    adds js html exercises

---
+++ js/external.js
@@ -0,0 +1,14 @@
+console.log("hello from external JavaScript");
+
+alert("Well to my Website!");
+
+var userInput = prompt('What is your favorite color?');
+console.log(alert('Awesome! ' + userInput + ' is my favorite color too!'));
+
+//---------------------------------------------------------------------------------------------------------------------
+
+var mermaid = prompt('How many days did you rent little mermaid for?')
+var bBear = 5 * 3
+var herc = 1 * 3
+
+prompt("Your total for your rentals is $" + (mermaid + bBear + herc));
