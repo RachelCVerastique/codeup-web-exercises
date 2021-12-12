ae0dd888d2bcb69b6fc42cc9c922323a2672d43e
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2021-12-12 00:44:29 -0600

    adds js exercises

--- js/external.js
+++ js/external.js
@@ -1,14 +1,54 @@
-console.log("hello from external JavaScript");
+//console.log("hello from external JavaScript");
 
-alert("Well to my Website!");
+//alert("Well to my Website!");
 
-var userInput = prompt('What is your favorite color?');
-console.log(alert('Awesome! ' + userInput + ' is my favorite color too!'));
+//var userInput = prompt('What is your favorite color?');
+//console.log(alert('Awesome! ' + userInput + ' is my favorite color too!'));
 
 //---------------------------------------------------------------------------------------------------------------------
 
-var mermaid = prompt('How many days did you rent little mermaid for?')
-var bBear = 5 * 3
-var herc = 1 * 3
+// var mermaidInput = parseInt(prompt('How many days did you rent little mermaid for?'));
+// var bBearInput = parseInt(prompt('How many days did you rent brother bear for?'));
+// var hercInput = parseInt(prompt('how many days did you rent hercules for?'));
+// var rentCharge = 3;
+// var rentCost = (mermaidInput + bBearInput + hercInput) * rentCharge;
+//
+//
+// var confirmed = confirm("Your total for your rentals is $" + (rentCost));
+//
+// console.log(confirmed)
 
-prompt("Your total for your rentals is $" + (mermaid + bBear + herc));
+//----------------------------------------------------------------------------------------------------------------------
+
+// var amzInputHrs = parseInt(prompt('How many hours did you work for Amazon?'))
+// var googInputHrs = parseInt(prompt('How many hours did you work for Google?'))
+// var fbInputHrs = parseInt(prompt('How many hours did you work for FaceBook?'))
+// var amzRt = 380, googRt = 400, fbRt = 350
+//
+//
+// var total = ((amzRt * amzInputHrs) + (googRt * googInputHrs) +  (fbRt * fbInputHrs))
+// var confirmPay = confirm("You will get paid $" + total)
+// console.log(total);
+
+//----------------------------------------------------------------------------------------------------------------------
+
+var timeInput =prompt( 'This course is at 10 a.m. , is there a schedule conflict present?');
+var studentsEnrolled = 17
+
+ if (timeInput.includes("No")) {
+     var studentInput = parseInt(prompt('Enter number of enrolling students.'))
+     // alert('Move on to further determine enrollment eligibility.');
+ } else {
+     (alert('Schedule conflict present ineligible to enroll'));
+ }
+ if (studentInput + studentsEnrolled < 22) {
+     alert('Student(s) enrollment successful!');
+} else { alert('ineligible for enrollment at this time.')}
+
+
+
+
+
+
+
+
