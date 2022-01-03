b0244b2d296d8ce82b65875de1a0179cd8c94db8
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2022-01-03 11:49:26 -0600

    adds js exercises

--- js/practice.js
+++ js/practice.js
@@ -60,3 +60,95 @@
 //      }
 //
 //  console.log(analyzeColor("purple"));
+
+
+// Make a function named isOdd(number)
+
+//function isOdd()
+
+
+
+
+// Make a function named isEven(number)
+// Make a function named identity(input) that returns the input exactly as provided.
+//     Make a function named isFive(input)
+// Make a function named addFive(input) that adds five to some input.
+
+
+
+// Maths!
+//     Write a function called add(num1, num2) which returns the sum of a and b
+
+//     function add(num1, num2) {
+//         return (num1 + num2);
+//     }
+//
+// console.log(add(1, 2))
+
+
+
+// Write a function named square(numToSqr) which takes in a number (numToSqr)
+// and returns the number multiplied by itself
+
+
+//     function square(numToSqr) {
+//         return numToSqr ** 2;
+//     }
+//
+// console.log(square(3))
+
+
+// Write a function called sumOfSquares(a, b).
+//     You should find the square of a, then the square of b.
+//     Invoke add(num1, num2) and pass the new squared values in as arguments.
+//
+// function sumOfSquares(a, b) {
+//     var sqOne = square(a);
+//     var sqTwo = square(b);
+//     return add(sqOne, sqTwo);
+// }
+//
+// console.log(sumOfSquares(2, 3))
+
+//
+// instructors example
+//     function sumOfSquares(a,b){
+//     return add(square(a), square(b));
+// }
+//
+// console.log(sumOfSquares(3, 2));
+//
+
+// FIZZ-BUZZZZZZZ
+// Write a function named getFizzBuzz(startingNum) which performs the following actions:
+//     If startingNum is divisible by 3
+// console log "fizz"
+// Else, if startingNum is divisible by 5
+// console log "buzz"
+// If startingNum is divisible by 3 AND divisible by 5
+// return a string which says "FIZZ BUZZ"
+// Thinking about getFizzBuzz(startingNum), let's console log the return value of this function
+// What if getFizzBuzz doesn't return a value (VOID)?
+// How do you want to handle that?
+
+
+    function getFizzBuzz(startingNum) {
+        if (startingNum % 3 === 0 && startingNum % 5 === 0) {
+            return "Fizz Buzz";
+        } else if (startingNum % 5 === 0) {
+            console.log("Buzz");
+        } else if (startingNum % 3 === 0) {
+            console.log("Fizz");
+        }
+    }
+console.log(getFizzBuzz(9))
+
+
+
+
+
+
+
+
+
+
