a724782fa265c2a1f20e0387dbe64a597213d981
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2022-01-19 22:47:17 -0600

    adds media queries test files and other exercises

--- js/practice.js
+++ js/practice.js
@@ -424,4 +424,36 @@
 //
 // console.log(removeAll(bugs, "ant"))
 
+// Write a JS function called getNthCat that takes an array of cat objects as a first parameter called cats, and n as a second parameter.
+//     The function returns the cat object at index n in the cats array.
+//     For example:
+//     var myCats = [
+//         {
+//             catName: "Fifi",
+//             age: 5
+//         },
+//         {
+//             catName: "Fluffy",
+//             age: 3
+//         },
+//         {
+//             catName: "Abby",
+//             age: 7
+//         }
+//     ];
+//
+// // print out the cat at index 1
+// console.log(getNthCat(myCats, 1));
 
+
+var cats = [
+        {catName: "Fifi", age: 5},
+        {catName: "Fluffy", age: 3},
+        {catName: "Abby", age: 7}
+];
+
+function getNthCat (cats, n) {
+    return cats[n];
+}
+
+console.log(getNthCat(cats, 2));
