f4896f6e811ec60fab7a871a6e7f76849a4f67a7
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2022-01-27 13:25:56 -0600

    adds dom exercise and js warm up/practice

--- js/practice.js
+++ js/practice.js
@@ -471,3 +471,19 @@
 // }
 //
 // findAverage([95, 74, 86, 100])
+
+
+// morning JS warmup:
+//     write a function called sumOfSquares(a, b)
+// takes 2 parameters called a and b
+// returns the sum of the squares of a and b
+// i.e., (a to the power of 2) + (b to the power of 2)
+// e.g., console.log(sumOfSquares(2, 3));
+// will return 13
+
+function sumOfSquares(a, b) {
+    return Math.pow(a, 2) Math.pow(b,2);
+
+}
+
+console.log(sumOfSquares(2,2));
