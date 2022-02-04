cb9446e6c5f6b723c2dcbccb9a5eab3c85dbb5fc
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2022-02-04 09:53:09 -0600

    adds js warmup

--- js/warmup.js
+++ js/warmup.js
@@ -27,3 +27,32 @@
 
 
 
+// Write a function called convertLowHighToObject that takes in a string
+// that represents the low and high temperature in this format '35, 42'
+// (assuming low will always be the left value and high the right)
+// and returns an object with the properties 'low' and 'high' with the values of those respective
+// properties set to the corresponding values in the input string.
+//     NOTE: the values in the object should be of the number type
+// Example input: '35, 42'
+// Example return: {low: 35, high: 42}
+
+
+//     function convertLowHighToObject(string1, string2) {
+//             if (parseInt(string1) < parseInt(string2)) {
+//                 return 'low:' + string1 + ',' + 'high:' + string2;
+//             } else if (parseInt(string2) < parseInt(string1))
+//         return 'low:' + parseInt(string2) + ',' + 'high:' + parseInt(string1);
+//     }
+//
+// console.log( convertLowHighToObject('87', '12'));
+
+// actual solution
+function convertLowHighToObject(lowHighStr) {
+    let lowHighArr = lowHighStr.split(', ');
+    return {
+        low: parseInt(lowHighArr[0]),
+        high: parseInt(lowHighStr[1])
+    }
+}
+
+console.log(convertLowHighToObject('12', '54'));
